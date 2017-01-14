663ea48b643a7fc60be9c30f6b61a5fe4a8157e1
Penghuan Ni <nixxx136@d.umn.edu>
2017-01-14 17:09:49 -0600

    Add problem 207 course schedule

---
+++ 193-256/207_course_schedule.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new LinkedList<List<Integer>>();
+        
+        // initialize adj with all empty linkedlist
+        for (int i=0; i<numCourses; i++) {
+            adj.add(new LinkedList<Integer>());
+        }
+        
+        // compute indegree for each node
+        for (int[] preq : prerequisites) {
+            int course = preq[0];
+            int prereq = preq[1];
+            indegree[course]++;
+            adj.get(prereq).add(course);
+        }
+        
+        // create a queue and add those indegree 0 nodes
+        Queue<Integer> qu = new LinkedList<>();
+        for (int i=0; i<numCourses; i++) {
+            if (indegree[i]==0) qu.add(i);
+        }
+        
+        int size=0;
+        while (!qu.isEmpty()) {
+            int tmp = qu.remove();
+            for (Integer node : adj.get(tmp)) {
+                if (--indegree[node]==0) qu.add(node);
+            }
+            size++;
+        }
+        return size==numCourses;
+    }
+}
