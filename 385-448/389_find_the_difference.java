b61122b420207b246da259b9f432f6c350e8faa9
Penghuan Ni <nipenghuan@gmail.com>
2017-01-15 12:00:44 -0600

    Add more comments for problem 389

--- 385-448/389_find_the_difference.java
+++ 385-448/389_find_the_difference.java
@@ -1,7 +1,12 @@
 public class Solution {
     public char findTheDifference(String s, String t) {
-        int ret = 0;
-        
+	// Basic idea for this problem is to add all the char in t,
+	// delete all the char in s, then the last will be the extra one.
+
+	// Instead of using add operator, I use XOR here. The reason is,
+	// when you apply XOR on one char twice, then you will get an 0.
+	int ret = 0;
+	
         for (char ch : s.toCharArray()) ret ^= ch;
         for (char ch : t.toCharArray()) ret ^= ch;
         
