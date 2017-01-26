2a1def88d4dd703bb152b439428262ab1286f188
Penghuan Ni <nipenghuan@gmail.com>
2017-01-26 13:23:56 -0600

    update problem 414 third maximum number

--- 385-448/414_third_maximum_number.java
+++ 385-448/414_third_maximum_number.java
@@ -1,32 +1,32 @@
 public class Solution {
     public int thirdMax(int[] nums) {
-        // initialize first, second, third to be Integer.MIN_VALUE
-        int first=Integer.MIN_VALUE, second=Integer.MIN_VALUE, third=Integer.MIN_VALUE;
+        // initialize first, second, third to be null
+        Integer first=null, second=null, third=null;
         
         // iterate through int array nums
-        for (int n : nums) {
+        for (Integer n : nums) {
             // first case: if n equals to one of those three values
             // skip it.
-            if (n==first || n==second || n==third) {
+            if (n.equals(first) || n.equals(second) || n.equals(third)) {
                 continue;
-            } else if (first==Integer.MIN_VALUE || n>first) { 
-                // second case: if n equals Integer.MIN_VALUE or greater that first
+            } else if (first==null || n>first) { 
+                // second case: if n equals null or greater that first
                 // update first, second, third
-                // if n equals Integer.MIN_VALUE, which means it has not been updated yet
+                // if n equals null, which means it has not been updated yet
                 third=second;
                 second=first;
                 first=n;
-            } else if (second==Integer.MIN_VALUE || n>second) {
-                // third case: if n equals Integer.MIN_VALUE or greater that first
+            } else if (second==null || n>second) {
+                // third case: if n equals null or greater that first
                 // update second, third
                 third=second;
                 second=n;
-            } else if (third==Integer.MIN_VALUE || n>third) {
-                // fourth case: if n equals Integer.MIN_VALUE or greater that first
+            } else if (third==null || n>third) {
+                // fourth case: if n equals null or greater that first
                 // update third
                 third=n;
             }
         }
-        return third==Integer.MIN_VALUE ? first : third;
+        return third==null ? first : third;
     }
 }
