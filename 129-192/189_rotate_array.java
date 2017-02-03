f1d0fc48139578a3cce003675c3eb0d54ed7a8a8
Penghuan Ni <nipenghuan@gmail.com>
2017-02-02 20:02:13 -0600

    189 rotate array

---
+++ 129-192/189_rotate_array.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        // in-place solution with O(1) extra space
+        k %= nums.length;
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+    }
+    
+    public void reverse(int[] nums, int start, int end) {
+        int tmp;
+        while (start<end) {
+            tmp=nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = tmp;
+        }
+    }
+}
